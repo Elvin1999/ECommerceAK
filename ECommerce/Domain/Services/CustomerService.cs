a4b9818d2b30b2e1027aeb1db301310b071118e6
camalzadeelvin <camalzadeelvin@STHQ0127-03>
2023-02-05 13:12:33 +0400

    Add project files.

---
+++ ECommerce/Domain/Services/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.DataAccess.SqlServer;
+using ECommerce.DataAccess.SqlServer.Repository;
+using ECommerce.Domain.Abstractions;
+using System.Linq;
+
+namespace ECommerce
+{
+    public class CustomerService
+    {
+        private readonly IRepository<Customer> _customerRepo;
+        public CustomerService()
+        {
+            _customerRepo = new CustomerRepository();
+        }
+
+        public Customer GetCustomerByUsername(string username)
+        {
+            var customer = _customerRepo.GetAllData().FirstOrDefault(c => c.Username == username);
+            return customer;
+        }
+    }
+}
---
+++ ECommerce/Domain/ViewModels/ProductInfoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.DataAccess.SqlServer;
+using ECommerce.DataAccess.SqlServer.Repository;
+using ECommerce.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.ViewModels
+{
+    public class ProductInfoViewModel:BaseViewModel
+    {
+        private readonly IRepository<Product> _productRepo;
+        private readonly IRepository<Order> _orderRepo;
+        private readonly CustomerService _customerService;
+
+        public ProductInfoViewModel()
+        {
+            _productRepo = new ProductRepository();
+            _orderRepo = new OrderRepository();
+            _customerService = new CustomerService();
+
+            ProductInfo = new Product();
+
+        }
+
+        private Product productInfo;
+
+        public Product ProductInfo
+        {
+            get { return productInfo; }
+            set { productInfo = value; OnPropertyChanged(); }
+        }
+
+    }
+}
